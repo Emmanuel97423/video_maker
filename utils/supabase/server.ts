2d9d1b52d67b0f6a1ddec0286b97796c6d7c2a5d
enarcisse <en@infoport.re>
2025-02-28 08:32:13 +0400

    [feat/supabase-google-auth]: fix

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,5 +1,5 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr'
-import { cookies } from 'next/headers'
+import {createServerClient} from '@supabase/ssr'
+import {cookies} from 'next/headers'
 
 export async function createClientForServer() {
     const cookieStore = await cookies();
